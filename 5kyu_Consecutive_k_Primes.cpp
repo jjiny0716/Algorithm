32661c631b865a4027d3f945264f82aff6197235
jjiny <jjiny3773@naver.com>
2021-07-29 15:08:40 +0900

    Add solutions

---
+++ 5kyu_Consecutive_k_Primes.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <cmath>
+using namespace std;
+#include <iostream>
+class PrimeConsec
+{
+private:
+    static int countPrimeDiv(long long n);
+public:
+    static int consecKprimes(int k, vector<long long>& arr) {
+        int isLastKPrime = 0;
+        int result = 0;
+        for (long long n : arr) {
+            vector<int> factors;
+            long long dp = 2;
+            while (n != 1) {
+                if (n % dp == 0) {
+                    n /= dp;
+                    factors.push_back(dp);
+                }
+                else { // 다음 소수로 넘어가기;
+                    while (true) {
+                        int isPrime = 1;
+                        dp++;
+                        for (int i = 2; i <= sqrt(dp); i++) {
+                            if (dp % i == 0) isPrime = 0;
+                        }
+                        if (isPrime == 0) continue;
+                        else break;
+                    }
+                }
+            }
+            //cout << n << endl;
+            //for (int f : factors) cout << f << " ";
+            //cout << endl;
+            if (isLastKPrime == 1 && factors.size() == k) result++;
+            if (factors.size() == k) isLastKPrime = 1;
+            else isLastKPrime = 0;
+        }
+
+        return result;
+    }
+};
+
+
+int main()
+{
+    vector<long long> t = { 10175, 10185, 10180, 10197 };
+    cout << PrimeConsec::consecKprimes(4, t);
+
+    return 0;
+}
--- 6kyu_How_Much.cpp
+++ 6kyu_How_Much.cpp
@@ -30,7 +30,7 @@ public:
 
 		result += '[';
 		for (vector<int> v : cases) { // ["M: 1045", "B: 149", "C: 116"]
-			result += "[M: " + to_string(v[0]) + " B : " + to_string(v[1]) + " C : " + to_string(v[2]) + "]";
+			result += "[M: " + to_string(v[0]) + " B: " + to_string(v[1]) + " C: " + to_string(v[2]) + "]";
 			
 		}
 		//if (!cases.empty()) result.erase(result.size() - 1, 1);
@@ -47,3 +47,38 @@ int main()
 	
 	return 0;
 }
+
+
+// other answer
+#include <algorithm>
+
+class Carboat
+{
+
+public:
+	static std::string howmuch(int m, int n)
+	{
+		int min = std::min(m, n);
+		int max = std::max(m, n);
+
+		int num = min + 37 - (min % 63);
+		num = (num >= min) ? num : num + 63;
+
+		/*
+		 *  There is no need to check every
+		 *  value. As the cars need to be multiple
+		 *  of 9 and boats multiple of 7 it is clear
+		 *  that the answers are always 63 units apart.
+		 */
+		std::string answer = "[";
+		for (; num <= max; num += 63)
+		{
+			answer += "[M: " + std::to_string(num) +
+				" B: " + std::to_string((num - 2) / 7) +
+				" C: " + std::to_string((num - 1) / 9) + "]";
+		}
+		answer += "]";
+
+		return answer;
+	}
+};
