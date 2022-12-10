040642a178da4b2e9c089c7d102881d53366e298
jjiny0716 <jjiny3773@naver.com>
2022-12-10 19:31:47 +0900

    Add solutions

---
+++ leetcode/377. Combination Sum IV.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function (nums, target) {
+  const countFromSum = new Array(target + 1).fill(-1);
+
+  const makeAllCombinationToTarget = (sum = 0) => {
+    if (sum > target) return 0;
+    else if (sum === target) return 1;
+    else if (countFromSum[sum] !== -1) {
+      return countFromSum[sum];
+    }
+
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+      count += makeAllCombinationToTarget(sum + nums[i], i);
+    }
+
+    return (countFromSum[sum] = count);
+  };
+  makeAllCombinationToTarget();
+
+  return countFromSum[0];
+};
+
+const nums = [
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170,
+    180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320,
+    330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470,
+    480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620,
+    630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 760, 770,
+    780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910, 920,
+    930, 940, 950, 960, 970, 980, 990, 111,
+  ],
+  target = 999;
+console.log(combinationSum4(nums, target));
