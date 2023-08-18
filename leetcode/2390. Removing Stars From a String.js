b754879bdb4b52da15624dff9ea675f37994958d
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-08-18 23:13:39 +0900

    Add solutions

---
+++ leetcode/2390. Removing Stars From a String.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+  const stack = [];
+  
+  for (const c of s) {
+    if (c === '*') stack.pop();
+    else stack.push(c);
+  }
+
+  return stack.join('');
+};
+
+const s = "leet**cod*e";
+console.log(removeStars(s));
